5da4f9272fe749d1f0be7cd7be6a65c395f0a41f
James Jin <etimes001@gmail.com>
2016-01-08 01:50:47 +1300

    finish question2

--- question_2.php
+++ question_2.php
@@ -21,3 +21,47 @@ abstract class Customer {
 
 
 // Write your code below
+/**
+ * Class Bronze_Customer
+ * @package SoftwareEngineerTest
+ */
+class Bronze_Customer extends Customer {
+
+	/**
+	 * Bronze customers do not receive any extra credits
+	 * @param $amount
+	 */
+	public function deposit($amount){
+		$this->balance = $this->get_balance() + $amount;
+	}
+}
+
+/**
+ * Class Silver_Customer
+ * @package SoftwareEngineerTest
+ */
+class Silver_Customer extends Customer {
+
+	/**
+	 * Silver customers get 5% extra credit added their account balance
+	 * @param $amount
+	 */
+	public function deposit($amount){
+		$this->balance = $this->get_balance() + $amount * (1.05);
+	}
+}
+
+/**
+ * Class Gold_Customer
+ * @package SoftwareEngineerTest
+ */
+class Gold_Customer extends Customer {
+
+	/**
+	 * Gold customers get 10% extra credit added to their account balance
+	 * @param $amount
+	 */
+	public function deposit($amount){
+		$this->balance = $this->get_balance() + $amount * (1.1);
+	}
+}
